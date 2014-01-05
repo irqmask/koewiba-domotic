ec47d12eb91f70b299adcd0886b50e34e0fab36e
robert <robert@17cfe68c-0646-0410-b711-8d3b7ae1a5c4>
2014-01-05 20:09:49 +0000

    Debug-LED defines moved here from led_debug.h.

--- sw/src/taster8/pcbconfig.h
+++ sw/src/taster8/pcbconfig.h
@@ -25,6 +25,23 @@
 #define BUS_PORT_DISRCV PORTD
 #define BUS_DISRCV      PD3
 
+// Pin assignments of board keys and LEDs
+// Port D pin assignments
+#define LED_STATUS      0b01000000  //!< Yellow status LED
+#define LED_ERROR       0b00010000  //!< Red error LED
+#define BTN_TEST        0b00100000
+#define BTN_EXP         0b10000000
+
+#if defined (__AVR_ATmega88__) || defined (__AVR_ATmega88A__) || defined (__AVR_ATmega8__)
+#define LED_STATUS_ON       PORTD |=  LED_STATUS
+#define LED_STATUS_OFF      PORTD &= ~LED_STATUS
+#define LED_STATUS_TOGGLE   PORTD ^=  LED_STATUS
+
+#define LED_ERROR_ON        PORTD |=  LED_ERROR
+#define LED_ERROR_OFF       PORTD &= ~LED_ERROR
+#define LED_ERROR_TOGGLE    PORTD ^=  LED_ERROR
+#endif
+
 // --- Type definitions --------------------------------------------------------
 
 // --- Local variables ---------------------------------------------------------

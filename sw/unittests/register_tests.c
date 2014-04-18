0a97e6536e0fb9249526bc2af4f0e185b63550a8
Christian Verhalen <christian@MobilLnx01.localdomain>
2014-04-18 21:34:49 +0200

    Added scheduler unittests.

--- sw/unittests/register_tests.c
+++ sw/unittests/register_tests.c
@@ -1,11 +1,13 @@
 #include <CUnit/CUnit.h>
 
+extern CU_TestInfo          TESTBUSSCHED_asTests[];
 extern CU_TestInfo          TESTPWM_asTests[];
 extern CU_InitializeFunc    PWMTEST_iSuiteInit;
 extern CU_CleanupFunc       PWMTEST_iSuiteCleanup;
 
 CU_SuiteInfo TEST_asSuites[] = {
-    { "PWM", NULL, NULL, TESTPWM_asTests },
+	{ "BUSSCHED",   NULL,   NULL,   TESTBUSSCHED_asTests },
+    { "PWM",        NULL,   NULL,   TESTPWM_asTests },
     //{ NULL, PWMTEST_iSuiteInit, PWMTEST_iSuiteCleanup, TESTPWM_asTests },
     CU_SUITE_INFO_NULL,
 };

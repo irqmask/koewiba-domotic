c6eee8a91f3643ff06bf34a38ea488f2cf1e04a9
christian <irqmask@web.de>
2014-10-16 12:48:11 +0200

    led_debug adjusted to new coding style.

--- sw/src/shared/led_debug.c
+++ sw/src/shared/led_debug.c
@@ -1,10 +1,10 @@
 /**
- * @addtogroup TEST_PROTOCOL
+ * @addtogroup DEBUG
+ * @brief   Debug output via LEDs.
  *
  * @{
  * @file    led_debug.c
- * @brief   TODO describe briefly.
- * @todo    describe file purpose
+ * @brief   Debug output via LEDs.
  * @author  Robert Mueller
  *///---------------------------------------------------------------------------
 
@@ -13,10 +13,7 @@
 #include "ucontroller.h"
 #include <avr/interrupt.h>
 
-
 // --- Definitions -------------------------------------------------------------
-
-
 
 // --- Type definitions --------------------------------------------------------
 
@@ -28,32 +25,35 @@
 
 // --- Local functions ---------------------------------------------------------
 
-
 /**
  * Toggle the status LED.
  */
-void vToggleStatusLED(void)
+void dbg_toggle_status_LED (void)
 {
     PORTD ^= LED_STATUS;
 }
-void vStatusLED_on(void)
+
+void dbg_status_LED_on (void)
 {
     PORTD |= LED_STATUS;
 }
-void vStatusLED_off(void)
+
+void dbg_status_LED_off (void)
 {
     PORTD &= ~LED_STATUS;
 }
 
-void vToggleErrorLED(void)
+void dbg_toggle_error_LED (void)
 {
     PORTD ^= LED_ERROR;
 }
-void vErrorLED_on(void)
+
+void dbg_error_LED_on (void)
 {
     PORTD |= LED_ERROR;
 }
-void vErrorLED_off(void)
+
+void dbg_error_LED_off (void)
 {
     PORTD &= ~LED_ERROR;
 }
@@ -61,9 +61,3 @@ void vErrorLED_off(void)
 // --- Module global functions -------------------------------------------------
 
 // --- Global functions --------------------------------------------------------
-#if defined (__AVR_ATmega88__)
-ISR(INTERRUPT_PINCHANGE2)
-{
-
-}
-#endif

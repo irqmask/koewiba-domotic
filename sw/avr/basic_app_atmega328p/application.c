0336a5d26269048f2328e6f9a97a20ad5ff4d388
christian <irqmask@web.de>
2016-03-24 18:40:38 +0100

    introduced common main_avr module used in basic_app_atmega328

---
+++ sw/avr/basic_app_atmega328p/application.c
@@ -0,0 +1,71 @@
+/**
+ * @addtogroup BASIC_APP_ATMEGA328
+ * @addtogroup APPLICATION
+ * @brief Application specific code of "basic_app_atmega328" project.
+ *
+ * Contains application specific initialization, command-interpreter, 
+ * register code and background loop.
+ * 
+ * @{
+ * @file    application.c
+ * @brief   Application specific code of "basic_app_atmega328" project.
+ *
+ * @author  Christian Verhalen
+ *///---------------------------------------------------------------------------
+
+// --- Include section ---------------------------------------------------------
+
+#include "prjtypes.h"
+
+// --- Definitions -------------------------------------------------------------
+
+// --- Type definitions --------------------------------------------------------
+
+// --- Local variables ---------------------------------------------------------
+
+// --- Global variables --------------------------------------------------------
+
+// --- Module global variables -------------------------------------------------
+
+// --- Local functions ---------------------------------------------------------
+
+// --- Module global functions -------------------------------------------------
+
+// --- Global functions --------------------------------------------------------
+
+/**
+ * Application specific initializations.
+ * 
+ * Executed if common code initialization passed.
+ * @note Global interrupts are still switched off! Will be switched on in global
+ * main after this initialization code.
+ */
+void app_init (void) 
+{
+    //TODO insert application specific initializations here!
+}
+
+/**
+ * Application specific command interpreter code.
+ * 
+ * Executed if a not-common command is received.
+ */
+void app_on_command (uint16_t sender, uint8_t msglen, uint8_t* msg)
+{
+    switch (msg[0]) {
+    //TODO insert application specific command interpreter here!
+    default:
+        break;
+    }
+}
+
+/**
+ * Application specific background code.
+ * 
+ * Executed once per main loop cycle.
+ */
+void app_background (void)
+{
+    //TODO insert application specific background routines here!
+}
+/** @} */

6b4a21086e46dbba5d07f035f5816624c9bb42d9
christian <irqmask@web.de>
2016-03-24 14:02:04 +0100

    Updated build targets for complete build of AVR projects

--- sw/avr/buildscripts/targets.mk
+++ sw/avr/buildscripts/targets.mk
@@ -1,3 +1,3 @@
-TARGETS = bootloader_atmega324p roomthermostate taster8 tests/testscheduler
+TARGETS = basic_app_atmega324p basic_app_atmega328p bootloader_atmega324p bootloader_atmega328p roomthermostate scheduler_attiny1634 taster8  tests/elektor_bus-node_atmega88 tests/test_bus-module_atmega324p tests/test_bus-module_atmega328p tests/test_bus-module_attiny1634 tests/testgateway232_atmega324p
 
 SUBDIRS = $(addprefix ../,$(TARGETS))

6c85237fe47ee3dfcce8ae10566a7a15b0f7e869
verhalc <verhalc>
2016-05-19 15:21:07 +0200

    Add windows version of getopt (from https://gist.github.com/superwills/5815344)

---
+++ sw/os/libsystem/getopt.h
@@ -0,0 +1,21 @@
+#ifndef _GETOPT_H_
+#define _GETOPT_H_
+
+#include "prjconf.h"
+
+#if defined (PRJCONF_WINDOWS)
+#if defined(_MSC_VER) && _MSC_VER < 1900
+
+// windows portion of getopt is taken from:
+// https://gist.github.com/superwills/5815344
+
+extern char *optarg;
+
+int getopt(int nargc, char * const nargv[], const char *ostr);
+
+#endif // defined(_MSC_VER) && _MSC_VER < 1900
+#else
+#include <getopt.h>
+#endif // defined (PRJCONF_WINDOWS)
+
+#endif

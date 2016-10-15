605a2de28f6d084f43b63c671361f77ae5e7742a
christian <irqmask@web.de>
2016-10-15 17:16:47 +0200

    Windows uses _getch(), Linux uses getch()

--- sw/os/libsystem/sysconsole.h
+++ sw/os/libsystem/sysconsole.h
@@ -10,7 +10,13 @@
   #include <stdarg.h>
 #endif
 
-#define sys_con_getch() _getch()
+#if defined (PRJCONF_UNIX) || \
+    defined (PRJCONF_POSIX) || \
+    defined (PRJCONF_LINUX)
+  #define sys_con_getch() getch()
+#elif defined (PRJCONF_WINDOWS) 
+  #define sys_con_getch() _getch()
+#endif
 
 #if defined (PRJCONF_UNIX) || \
     defined (PRJCONF_POSIX) || \

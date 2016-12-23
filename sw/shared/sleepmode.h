a5321797ca5b9f72cf20bddb6fb9508591c14b83
irqmask <irqmask@gmx.de>
2016-12-23 23:24:52 +0100

    sleepmode for all pinchange groups

--- sw/shared/sleepmode.h
+++ sw/shared/sleepmode.h
@@ -29,11 +29,11 @@
 #ifdef PRJCONF_UC_AVR
 
 #ifdef __AVR_ATtiny1634__
- #define sleep_pinchange2_enable()   GIMSK |=  (1<<PCIE2)
- #define sleep_pinchange2_disable()  GIMSK &= ~(1<<PCIE2)
+ #define sleep_pinchange_enable()    GIMSK |=  (1<<PCIE2)
+ #define sleep_pinchange_disable()   GIMSK &= ~(1<<PCIE2)
 #else
- #define sleep_pinchange2_enable()   PCICR |=  (1<<PCIE2)
- #define sleep_pinchange2_disable()  PCICR &= ~(1<<PCIE2)
+ #define sleep_pinchange_enable()    PCICR |=  ((1<<PCIE2) | (1<<PCIE1) | (1<<PCIE0))
+ #define sleep_pinchange_disable()   PCICR &= ~((1<<PCIE2) | (1<<PCIE1) | (1<<PCIE0))
 #endif
 // future use
 //#define sleep_pinchange2_enable()   PINCHGIR_REGISTER |=  PCIE_BITS
@@ -42,8 +42,8 @@
 #define sleep_activate()            sleep_mode()
 #define sleep_delay_ms(t)           _delay_ms(t)
 #else
-#define sleep_pinchange2_enable()
-#define sleep_pinchange2_disable()
+#define sleep_pinchange_enable()
+#define sleep_pinchange_disable()
 #define sleep_set_mode(m)
 #define sleep_activate()
 #define sleep_delay_ms(t)

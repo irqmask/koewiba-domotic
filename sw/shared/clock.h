d31b335d94b39cd00363117ec3fd067b5a89bd6c
christian <irqmask@web.de>
2017-01-14 17:21:57 +0100

    first working version of blindcontrol (clock.h was missing)

--- sw/shared/clock.h
+++ sw/shared/clock.h
@@ -21,8 +21,6 @@
 
 // --- Definitions -------------------------------------------------------------
 
-#define CLOCK_NUM_TIMER         4
-
 //! Convert from milliseconds into ticks.
 #define CLOCK_MS_2_TICKS(ms)    (uint16_t)((uint32_t)((uint32_t)ms*CLOCK_TICKS_PER_SECOND/1000))
 
@@ -30,6 +28,7 @@
 
 //! Time-out timer runtime-data.
 typedef struct clktimer {
+    uint16_t          tick_start_value; //!< start value of time count down.
     volatile uint16_t ticks;	//!< tick count down.
     volatile bool     active;   //!< timer is running.
 } clock_timer_t;
@@ -57,6 +56,10 @@ BOOL clk_timer_stop        (clock_timer_t* timer_instance);
 BOOL clk_timer_is_elapsed  (clock_timer_t* timer_instance);
 
 BOOL clk_timer_is_running  (clock_timer_t* timer_instance);
+
+uint16_t clk_timer_get_initial_start_ticks (clock_timer_t* timer_instance);
+
+uint16_t clk_timer_get_elapsed_ticks (clock_timer_t* timer_instance);
 
 uint16_t clk_timers_next_expiration (void);
 

97e31d9df9d244277c4dc9ea18c557ec3f91515e
Christian <irqmask@gmx.de>
2017-03-13 20:23:26 +0100

    Version information added

--- sw/avr/gateway232_atmega324p/appversion.c
+++ sw/avr/gateway232_atmega324p/appversion.c
@@ -1,11 +1,11 @@
 /**
- * @addtogroup BASIC_ATMEGA328
+ * @addtogroup BUS_GATEWAY
  * @addtogroup APPVERSION
- * @brief Version information of "basic_app_atmega328" project.
+ * @brief Version information of "gateway232_atmega324" project.
  *
  * @{
  * @file    appversion.c
- * @brief   Version information of "basic_app_atmega328" project.
+ * @brief   Version information of "gateway232_atmega324" project.
  *
  * @author  Christian Verhalen
  *///---------------------------------------------------------------------------
@@ -29,9 +29,9 @@
 //! @see BOARD_IDs, APP_IDs, APP_VERSIONING
 const unsigned char app_versioninfo[MOD_LEN_CONTROLLERID + MOD_LEN_BOARDID + MOD_LEN_BOARDREV + MOD_LEN_APPID + MOD_LEN_APPVER] __attribute__((section(".versioninfo")))
                         = {SIGNATURE_0,SIGNATURE_1,SIGNATURE_2,0x00,
-                           0x00,0x05,   // board ID            (high byte, low byte)
-                           2,           // board revision
-                           0x00,0x06,   // application ID      (high byte, low byte)
+                           0x00,0x01,   // board ID            (high byte, low byte)
+                           1,           // board revision
+                           0x00,0x02,   // application ID      (high byte, low byte)
                            0,1,0};      // application version (major, minor, bugfix)
 
 // --- Module global variables -------------------------------------------------

1b6ea1aaa5d5f1e4999c49e21ada32fbf37cb403
christian <irqmask@web.de>
2018-03-31 09:28:15 +0200

    pushbutton8 software initial working demo version with fixed receiver and commands

--- sw/avr/pushbutton8/pcbconfig.h
+++ sw/avr/pushbutton8/pcbconfig.h
@@ -30,15 +30,72 @@
 #define SPI_SCK_PIN         PC1
 
 // Pin assignments of board keys and LEDs
-// Port D pin assignments
-#define LED_PCBCONFIG       1
-#define LED_STATUS          PD6  //!< Yellow status LED
-#define LED_ERROR           PD4  //!< Red error LED
-#define BTN_TEST            PD5
-#define BTN_EXP             PD7
+#define LEDSKEYS_PCBCONFIG  1
+#define KEY0_DDR            DDRD
+#define KEY0_PORT           PORTD
+#define KEY0_IPORT          PIND
+#define KEY0_PIN            PD3
+#define KEY1_DDR            DDRC
+#define KEY1_PORT           PORTC
+#define KEY1_IPORT          PINC
+#define KEY1_PIN            PC4
+#define KEY2_DDR            DDRC
+#define KEY2_PORT           PORTC
+#define KEY2_IPORT          PINC
+#define KEY2_PIN            PC2
+#define KEY3_DDR            DDRC
+#define KEY3_PORT           PORTC
+#define KEY3_IPORT          PINC
+#define KEY3_PIN            PC0
+#define KEY4_DDR            DDRD
+#define KEY4_PORT           PORTD
+#define KEY4_IPORT          PIND
+#define KEY4_PIN            PD4
+#define KEY5_DDR            DDRC
+#define KEY5_PORT           PORTC
+#define KEY5_IPORT          PINC
+#define KEY5_PIN            PC5
+#define KEY6_DDR            DDRC
+#define KEY6_PORT           PORTC
+#define KEY6_IPORT          PINC
+#define KEY6_PIN            PC3
+#define KEY7_DDR            DDRC
+#define KEY7_PORT           PORTC
+#define KEY7_IPORT          PINC
+#define KEY7_PIN            PC1
 
-#define LED_STATUS_PORT     PORTD
-#define LED_ERROR_PORT      PORTD
+#define KEY0_WAKEUP         PCINT19 // PD3
+#define KEY0_WAKEUP_REG     PCMSK2
+#define KEY1_WAKEUP         PCINT12 // PC4
+#define KEY1_WAKEUP_REG     PCMSK1
+#define KEY2_WAKEUP         PCINT10 // PC2
+#define KEY2_WAKEUP_REG     PCMSK1
+#define KEY3_WAKEUP         PCINT8  // PC0
+#define KEY3_WAKEUP_REG     PCMSK1
+#define KEY4_WAKEUP         PCINT20 // PD4
+#define KEY4_WAKEUP_REG     PCMSK2
+#define KEY5_WAKEUP         PCINT13 // PC5
+#define KEY5_WAKEUP_REG     PCMSK1
+#define KEY6_WAKEUP         PCINT11 // PC3
+#define KEY6_WAKEUP_REG     PCMSK1
+#define KEY7_WAKEUP         PCINT9  // PC1
+#define KEY7_WAKEUP_REG     PCMSK1
+
+
+#define LED_OE_DDR          DDRD
+#define LED_OE_PORT         PORTD
+#define LED_OE_PIN          PD6
+#define LED_STB_DDR         DDRD
+#define LED_STB_PORT        PORTD
+#define LED_STB_PIN         PD5
+#define LED0                6
+#define LED1                5
+#define LED2                0
+#define LED3                4
+#define LED4                7
+#define LED5                3
+#define LED6                2
+#define LED7                1
 
 // --- Type definitions --------------------------------------------------------
 

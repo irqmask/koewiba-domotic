54808a0183cf0784cb4d7d792d3ae646f0499b48
christian <irqmask@web.de>
2018-06-07 15:37:02 +0200

    Feat: Add alarmclock and datetime module to blind-control_atmega328p. Feat: Add registers for alarmclock and datetime module's configuration Fix: Only move blinds on alarmclock events when blinds are idle. Doc: Add GPL2 license text to changed files.

--- sw/avr/blind-control_atmega328p/application.c
+++ sw/avr/blind-control_atmega328p/application.c
@@ -6,24 +6,34 @@
  * Contains application specific initialization, command-interpreter, 
  * register code and background loop.
  * 
- * @todo Implement control of motor relays
- * @todo Implement blind statemachines
- * @todo Implement key-debounce
- * @todo Implement window open/close notifications
- * @todo Implement date and time functions
- * @todo Implement general register receive functions
- *
  * @{
  * @file    application.c
  * @brief   Application specific code of "blind-control_atmega328p" project.
  *
  * @author  Christian Verhalen
  *///---------------------------------------------------------------------------
+/*
+ * Copyright (C) 2018  christian <irqmask@web.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 // --- Include section ---------------------------------------------------------
 
 #include "prjtypes.h"
 
+#include "alarmclock.h"
 #include "blindctrl.h"
 #include "bus.h"
 #include "cmddef_common.h"
@@ -137,6 +147,26 @@ void app_background (sBus_t* bus)
     if (timer_is_elapsed(&g_seconds_timer)) {
         timer_start(&g_seconds_timer, TIMER_MS_2_TICKS(1000));
         dt_tick_second();
+    }
+}
+
+/**
+ *  Check every minute if one of the up/down alarms triggers.
+ */
+void app_on_minute(void)
+{
+    int8_t alarm_idx = -1;
+    bool move_up = false;
+
+    if (alarm_check(&alarm_idx)) {
+        if (blind_is_moving(0) == false) {
+            move_up = ((alarm_idx % 2) == 0);
+            if (move_up) {
+                blind_move_to_position(0, 0);
+            } else {
+                blind_move_to_position(0, 100);
+            }
+        }
     }
 }
 

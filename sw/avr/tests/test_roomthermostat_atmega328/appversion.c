c4c0343e9410514f41aea1ddfd6a2799b3f06ff8
christian <irqmask@web.de>
2019-02-20 21:06:09 +0100

    set board id, app id , board revision and app version

--- sw/avr/tests/test_roomthermostat_atmega328/appversion.c
+++ sw/avr/tests/test_roomthermostat_atmega328/appversion.c
@@ -48,7 +48,7 @@ const unsigned char app_versioninfo[MOD_LEN_CONTROLLERID + MOD_LEN_BOARDID + MOD
                            0x00,0x06,   // board ID            (high byte, low byte)
                            2,           // board revision
                            0x00,0x07,   // application ID      (high byte, low byte)
-                           0,2,0};      // application version (major, minor, bugfix)
+                           0,2,1};      // application version (major, minor, bugfix)
 
 // --- Module global variables -------------------------------------------------
 

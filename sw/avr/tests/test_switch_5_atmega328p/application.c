51fd7aab8cc86f6df4d935b48185dbe0cce37228
christian <irqmask@web.de>
2021-03-03 13:00:26 +0100

    add switch5 test firmware

---
+++ sw/avr/tests/test_switch_5_atmega328p/application.c
@@ -0,0 +1,137 @@
+/**
+ * @addtogroup TEST_SWITCH_5_ATMEGA328
+ * @addtogroup APPLICATION
+ * @brief Application specific code of "test_switch_5_atmega328" project.
+ *
+ * Test hardware: eeprom, inputs, outputs. Therefore connect input 0 - 4 to
+ * output 0 - 4.
+ * 
+ * @{
+ * @file    application.c
+ * @brief   Application specific code of "test_switch_5_atmega328" project.
+ *
+ * @author  Robert Mueller
+ *///---------------------------------------------------------------------------
+/*
+ * Copyright (C) 2019  robert <djrfm@gmx.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+// --- Include section ---------------------------------------------------------
+
+#include "prjtypes.h"
+
+#include "eeprom_spi.h"
+#include "register.h"
+#include "sleepmode.h"
+
+// --- Definitions -------------------------------------------------------------
+
+#define TEST_EEPROM_MASK    (1<<0)  //!< bit is set if EEProm test succeeds
+#define TEST_RELAY_MASK     (1<<1)  //!< bit is set if relay test succeeds
+
+// --- Type definitions --------------------------------------------------------
+
+// --- Local variables ---------------------------------------------------------
+
+static uint8_t g_test_result_msg[2];
+
+// --- Global variables --------------------------------------------------------
+
+// --- Module global variables -------------------------------------------------
+
+// --- Local functions ---------------------------------------------------------
+
+// --- Module global functions -------------------------------------------------
+
+extern void    test_eeprom_start(void);
+extern uint8_t test_eeprom_run(void);
+extern uint8_t test_eeprom_is_ok(void);
+
+extern void test_io_initialize(void);
+extern uint8_t test_io_run(void);
+extern uint8_t test_io_is_ok(void);
+
+// --- Global functions --------------------------------------------------------
+
+/**
+ * Application specific initializations.
+ * 
+ * Executed if common code initialization passed.
+ * @note Global interrupts are still switched off! Will be switched on in global
+ * main after this initialization code.
+ */
+void app_init (void) 
+{
+    //TODO insert application specific initializations here!
+    //register_set_u16(MOD_eReg_ModuleID, 0x20);
+    test_eeprom_start();
+	test_io_initialize();
+
+    g_test_result_msg[0] = 0xEF; // currently unused command number
+    g_test_result_msg[1] = 0;
+
+    sleep_prevent(TEST_EEPROM_MASK | TEST_RELAY_MASK, true);
+}
+
+/**
+ * Application specific command interpreter code.
+ * 
+ * Executed if a not-common command is received.
+ */
+void app_on_command (uint16_t sender, uint8_t msglen, uint8_t* msg)
+{
+    switch (msg[0]) {
+    // no commands are processed
+    default:
+        break;
+    }
+}
+
+/**
+ * Application specific background code.
+ * 
+ * Executed once per main loop cycle.
+ */
+void app_background (sBus_t* g_bus)
+{
+	uint8_t eep_test_running, io_test_running;
+
+	eep_test_running = test_eeprom_run();
+	if (eep_test_running == 0) {
+	    sleep_prevent(TEST_EEPROM_MASK, false);
+
+		if (test_eeprom_is_ok() == 0) {
+		    g_test_result_msg[1] &= ~TEST_EEPROM_MASK;
+		} else {
+		    g_test_result_msg[1] |= TEST_EEPROM_MASK;
+		}
+		// report EEProm test status
+		bus_send_message(g_bus, 0x0000, sizeof(g_test_result_msg), g_test_result_msg);
+	}
+
+	io_test_running = test_io_run();
+	if (io_test_running == 0) {
+        sleep_prevent(TEST_RELAY_MASK, false);
+
+        if (test_io_is_ok() == 0) {
+		    g_test_result_msg[1] &= ~TEST_RELAY_MASK;
+		} else {
+		    g_test_result_msg[1] |= TEST_RELAY_MASK;
+		}
+		// report relay test status
+		bus_send_message(g_bus, 0x0000, sizeof(g_test_result_msg), g_test_result_msg); // send message to address 0x0002
+	}
+}
+/** @} */
---
+++ sw/avr/tests/test_switch_5_atmega328p/appregister.c
@@ -0,0 +1,119 @@
+/**
+ * @addtogroup BASIC_ATMEGA328
+ * @addtogroup APPREGISTER
+ * @brief Registers of the application "basic_app_atmega328".
+ *
+ * @{
+ * @file    appregister.h
+ * @brief   Registers of the application "basic_app_atmega328".
+ *
+ * @author  Christian Verhalen
+ *///---------------------------------------------------------------------------
+
+// --- Include section ---------------------------------------------------------
+
+#include <avr/boot.h>
+#include <avr/eeprom.h>
+
+#include "prjtypes.h"
+#include "ucontroller.h"
+#include "moddef_common.h"
+
+#include "register.h"
+
+#include "appconfig.h"
+
+// --- Definitions -------------------------------------------------------------
+
+// --- Type definitions --------------------------------------------------------
+
+// --- Local variables ---------------------------------------------------------
+
+// --- Global variables --------------------------------------------------------
+
+// --- Module global variables -------------------------------------------------
+
+// --- Local functions ---------------------------------------------------------
+
+// --- Module global functions -------------------------------------------------
+
+bool        app_register_get        (uint8_t                reg_no,
+                                     eRegType_t*            preg_type,
+                                     void*                  pvalue)
+{
+    eRegType_t  regtype;
+    uint8_t index;
+
+    if (preg_type == NULL) preg_type = &regtype;
+    if (pvalue == NULL) return false;
+    *preg_type = eRegType_U8;
+
+    // registers saved in EEProm
+    if (reg_no >= APP_eReg_RemoteAddr00 && reg_no <= APP_eReg_RemoteAddr31) {
+        index = (reg_no - APP_eReg_RemoteAddr00) * 2;
+        index += APP_eCfg_RemoteAddr00;
+        *(uint16_t*)pvalue = eeprom_read_word((uint16_t*)&register_eeprom_array[index]);
+        *preg_type = eRegType_U16;
+    }
+    else if (reg_no >= APP_eReg_RemoteReg00 && reg_no <= APP_eReg_RemoteReg31) {
+        index = reg_no - APP_eReg_RemoteReg00;
+        index += APP_eCfg_RemoteReg00;
+        *(uint8_t*)pvalue = eeprom_read_byte(&register_eeprom_array[index]);
+    }
+    else if (reg_no >= APP_eReg_TargetReg00 && reg_no <= APP_eReg_TargetReg31) {
+        index = reg_no - APP_eReg_TargetReg00;
+        index += APP_eCfg_TargetReg00;
+        *(uint8_t*)pvalue = eeprom_read_byte(&register_eeprom_array[index]);
+    }
+    else switch (reg_no) {
+    // registers saved in EEProm
+    // TODO add handler for with application specific registers here!
+
+    // registers in ROM/RAM
+    default:
+        return false;
+        break;
+    }
+    return true;
+}
+
+void        app_register_set        (uint8_t                reg_no,
+                                     uint32_t               value)
+{
+    uint16_t    tempval16;
+    uint8_t     tempval, index;
+
+    tempval16 = (uint16_t)(value & 0x0000FFFF);
+    tempval = (uint8_t)(value & 0x000000FF);
+
+    // registers saved in EEProm
+    if (reg_no >= APP_eReg_RemoteAddr00 && reg_no <= APP_eReg_RemoteAddr31) {
+        index = (reg_no - APP_eReg_RemoteAddr00) * 2;
+        index += APP_eCfg_RemoteAddr00;
+        eeprom_write_word((uint16_t*)&register_eeprom_array[index], tempval16);
+    }
+    else if (reg_no >= APP_eReg_RemoteReg00 && reg_no <= APP_eReg_RemoteReg31) {
+        index = reg_no - APP_eReg_RemoteReg00;
+        index += APP_eCfg_RemoteReg00;
+        eeprom_write_byte(&register_eeprom_array[index], tempval);
+    }
+    else if (reg_no >= APP_eReg_TargetReg00 && reg_no <= APP_eReg_TargetReg31) {
+        index = reg_no - APP_eReg_TargetReg00;
+        index += APP_eCfg_TargetReg00;
+        eeprom_write_byte(&register_eeprom_array[index], tempval);
+    }
+    else switch (reg_no) {
+    // registers saved in EEProm
+    // TODO add handler for with application specific registers here!
+
+
+    // registers in ROM/RAM
+
+    default:
+        break;
+    }
+}
+
+// --- Global functions --------------------------------------------------------
+
+/** @} */
